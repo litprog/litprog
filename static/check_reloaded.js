5144da29260aefe8da3711d9f4e9218798f65369
Manuel Barkhau <mbarkhau@gmail.com>
2019-04-12 15:13:13 +0200

    reorg: static files

---
+++ static/check_reloaded.js
@@ -0,0 +1,30 @@
+(function () {
+  var perf = window.performance;
+  if (perf && perf.navigation.type == perf.navigation.TYPE_RELOAD) {
+    var style = document.createElement("style");
+    style.textContent = "#below-the-fold {display: block;}";
+    document.head.appendChild(style);
+  }
+
+  var cl = document.body.classList;
+
+  var stored_font = localStorage.getItem("litprog_font")
+  if (stored_font != null) {
+    cl.remove("font-serif");
+    cl.remove("font-sans");
+    cl.remove("font-slab");
+    cl.add("font-" + stored_font);
+  }
+
+  var stored_theme = localStorage.getItem("litprog_theme")
+  var use_dark_theme = (
+    stored_theme !== null && stored_theme == "dark"
+    || matchMedia('(light-level: dim)').matches
+    || matchMedia('(prefers-color-scheme: dark)').matches
+  );
+  if (use_dark_theme) {
+    cl.add("dark");
+  } else {
+    cl.add("light");
+  }
+})();
---
+++ static/navigation.js
@@ -0,0 +1,83 @@
+(function() {
+  var cl = document.body.classList
+  function toggle_color_theme(e) {
+    if (cl.contains("dark")) {
+      cl.remove("dark");
+      cl.add("light");
+    } else {
+      cl.remove("light");
+      cl.add("dark");
+    }
+    cl.add("animate");
+    var prev_theme = localStorage.getItem("litprog_theme")
+    if (prev_theme == "dark") {
+        localStorage.setItem("litprog_theme", "light");
+    } else {
+        localStorage.setItem("litprog_theme", "dark");
+    }
+    e.stopPropagation();
+  }
+
+  function toggle_typeface(e) {
+    if (cl.contains("font-slab")) {
+      cl.remove("font-slab");
+      cl.add("font-serif");
+      localStorage.setItem("litprog_font", "serif");
+    } else if (cl.contains("font-serif")) {
+      cl.remove("font-serif");
+      cl.add("font-sans");
+      localStorage.setItem("litprog_font", "sans");
+    } else {
+      cl.remove("font-sans");
+      cl.add("font-slab");
+      localStorage.setItem("litprog_font", "slab");
+    }
+    e && e.stopPropagation();
+  }
+
+  document.getElementById("toggle-theme").addEventListener("click", toggle_color_theme);
+  document.getElementById("toggle-font").addEventListener("click", toggle_typeface);
+  document.getElementById("toggle-print").addEventListener("click", function(e) {
+    document.getElementById("pdf").classList.toggle("active")
+    e.stopPropagation();
+  });
+  document.getElementById("pdf").addEventListener("click", function(e) {
+    e.stopPropagation();
+  });
+  document.addEventListener('click', function(e) {
+    document.getElementById("pdf").classList.remove("active")
+  });
+
+  document.addEventListener('keydown', function(e) {
+    if (e.key == "Alt") {return;}
+    if (!e.altKey) {return;}
+
+    // var styles = window.getComputedStyle(document.body, "p")
+    // var line_height_str = styles.getPropertyValue("line-height");
+    // var line_height_px = parseInt(line_height_str.slice(0, -2));
+    //
+    // var extra_scroll = 0;
+    //
+    // if (e.key == "j") {extra_scroll = line_height_px * 3;}
+    // if (e.key == "J") {extra_scroll = line_height_px * 9;}
+    // if (e.key == "k") {extra_scroll = line_height_px * -3;}
+    // if (e.key == "K") {extra_scroll = line_height_px * -9;}
+    //
+    // if (extra_scroll !== 0) {
+    //     window.scrollBy(0, extra_scroll);
+    // }
+
+    if (e.key == "n") {
+        // Next Headline
+    }
+    if (e.key == "p") {
+        // Previous Headline
+    }
+    if (e.key == "t") {
+        toggle_typeface(e);
+    }
+    if (e.key == "c") {
+        toggle_color_theme(e);
+    }
+  });
+})();
