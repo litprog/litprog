e5d146a9d095a124e196dbc0deeeb91eb09b7560
Manuel Barkhau <mbarkhau@gmail.com>
2021-04-23 18:37:32 +0000

    update copyright headers

--- src/litprog/utils.py
+++ src/litprog/utils.py
@@ -1,7 +1,7 @@
 # This file is part of the litprog project
 # https://github.com/litprog/litprog
 #
-# Copyright (c) 2018-2020 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# Copyright (c) 2018-2021 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
 # SPDX-License-Identifier: MIT
 
 import typing as typ
