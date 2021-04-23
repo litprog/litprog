0136b7d12fd78941ce16962b2d84912c7c369578
Manuel Barkhau <mbarkhau@gmail.com>
2021-04-23 18:41:42 +0000

    don't use lib3to6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # This file is part of the litprog project
 # https://github.com/litprog/litprog
 #
-# Copyright (c) 2018-2020 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# Copyright (c) 2018-2021 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
 # SPDX-License-Identifier: MIT
 
 import os
@@ -35,21 +35,6 @@ long_description = "\n\n".join((read("README.md"), read("CHANGELOG.md")))
 
 
 package_dir = {"": "src"}
-
-is_lib3to6_fix_required = any(arg.startswith("bdist") for arg in sys.argv)
-
-if is_lib3to6_fix_required:
-    try:
-        import lib3to6
-        package_dir = lib3to6.fix(package_dir, target_version="3.6", install_requires=install_requires,)
-    except ImportError:
-        if sys.version_info < (3, 6):
-            raise
-        else:
-            sys.stderr.write((
-                "WARNING: Creating non-universal bdist, "
-                "this should only be used for development.\n"
-            ))
 
 
 static_globs = [
@@ -86,7 +71,6 @@ setuptools.setup(
         [console_scripts]
         lit=litprog.cli:cli
     """,
-
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -98,7 +82,6 @@ setuptools.setup(
         "Operating System :: POSIX",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development :: Libraries",
